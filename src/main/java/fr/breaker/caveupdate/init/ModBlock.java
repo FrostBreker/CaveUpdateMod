a9f1a1b437b19ca0e6c75018ebd5bd6357e25f17
FrostBreker <frostbreker@gmail.com>
2021-03-29 08:39:14 +0200

    Add Budding Amethyst Add Copper Ingot Furnace Recipe Add Candle Add Amethyst Cluster

--- src/main/java/fr/breaker/caveupdate/init/ModBlock.java
+++ src/main/java/fr/breaker/caveupdate/init/ModBlock.java
@@ -18,12 +18,21 @@ public class ModBlock
     public static final Block AMETHYST_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 2).requiresTool().strength(1.5f, 15f));
     public static final Block COPPER_ORE = new Block(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 1).requiresTool().strength(3f, 15f));
     public static final Block COPPER_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 2).requiresTool().strength(1.5f, 15f));
+    public static final Block BUDDING_AMETHYST = new Block(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 0).requiresTool().strength(1.5f, 1.5f).breakInstantly().dropsNothing().nonOpaque());
+    public static final Block CALCITE = new Block(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 0).requiresTool().strength(0.75f, 0.75f));
+    public static final Block AMETHYST_CLUSTER = new Block(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 2).requiresTool().strength(1.5f, 1.5f).nonOpaque().luminance(5));
+    public static final Block CANDLE = new Block(FabricBlockSettings.of(Material.STONE).strength(0.1f, 0.1f).luminance(12));
+
 
     public void registerAll()
     {
         register(AMETHYST_BLOCK, new Identifier(CaveUpdateMod.MODID, "amethyst_block"));
         register(COPPER_ORE, new Identifier(CaveUpdateMod.MODID, "copper_ore"));
         register(COPPER_BLOCK, new Identifier(CaveUpdateMod.MODID, "copper_block"));
+        register(BUDDING_AMETHYST, new Identifier(CaveUpdateMod.MODID, "budding_amethyst"));
+        register(AMETHYST_CLUSTER, new Identifier(CaveUpdateMod.MODID, "amethyst_cluster"));
+        register(CALCITE, new Identifier(CaveUpdateMod.MODID, "calcite"));
+        register(CANDLE, new Identifier(CaveUpdateMod.MODID, "candle"));
     }
 
     private void register(Block block, Identifier name)
